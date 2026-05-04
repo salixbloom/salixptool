329e8bafb8855e48283577fd5eac867c23b1b3fe
Ronan Bloom <bloomronan02@gmail.com>
2026-05-04 15:18:35 -0700

    Canvas features in progress

---
+++ src/Application/controllers/ApplicationController.java
@@ -0,0 +1,15 @@
+package Application.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ApplicationController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/Canvas/controllers/CanvasController.java
@@ -0,0 +1,27 @@
+package Canvas.controllers;
+
+import Canvas.CanvasBuffer;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CanvasController implements Initializable {
+
+
+    private ImageView canvas_view;
+    private CanvasBuffer myBuffer;
+
+    private boolean canvasExists;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        canvasExists = false;
+        myBuffer = null;
+    }
+
+    public void createCanvas() {
+
+    }
+}
